87b6ecd8a9823688f88c8823b6f8fc718281ea53
auyber <160794306+auyber@users.noreply.github.com>
2024-06-25 20:22:33 -0300

    feat: create about

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,5 +1,6 @@
 import NavBar from "../../components/NavBar/NavBar"
 import Hero from "../section/Hero/Hero"
+import About from "../section/About/About"
 
 
 const Home = () => {
@@ -9,6 +10,7 @@ const Home = () => {
     <>
       <NavBar />
       <Hero />
+      <About />
     </>
   )
 }
---
+++ src/pages/section/About/About.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { Box, Container, Typography, styled, Grid } from '@mui/material';
+
+const StyledAboutSection = styled(Box)(({ theme }) => ({
+  backgroundColor: '#ffffff',
+  padding: theme.spacing(8, 0),
+  textAlign: 'center',
+  marginTop: theme.spacing(4), // Adicionando margem superior para espaçamento
+}));
+
+
+const About: React.FC = () => {
+  return (
+    <StyledAboutSection>
+      <Container maxWidth="md">
+        <Grid container direction="column" alignItems="center" spacing={4}>
+          <Grid item>
+            <Typography variant="h2" component="h2">
+              Sobre Mim
+            </Typography>
+          </Grid>
+          <Grid item xs={12} md={10}>
+            <Typography variant="h5" textAlign="justify">
+              Como Operador Logístico em transição para a área da Tecnologia, estou animado para explorar novos horizontes e aplicar minhas habilidades analíticas e organizacionais em um novo campo. Com uma sólida experiência em Logística de Transporte, gerenciamento de tarefas de envio, coordenação de notas fiscais e escolha de modais de transporte, estou pronto para traduzir essas habilidades para o desenvolvimento de sistemas.
+              <br/><br/>
+              Formado em Logística Aeroportuária pela FATEC Indaiatuba e atualmente cursando Análise e Desenvolvimento de Sistemas, estou comprometido com minha transição de carreira. Meu foco atual está em me especializar como Desenvolvedor Full Stack, e em aprimorar minhas habilidades em análise de dados, visando assim oferecer soluções abrangentes e eficazes no campo da tecnologia.
+            </Typography>
+          </Grid>
+        </Grid>
+      </Container>
+    </StyledAboutSection>
+  );
+};
+
+export default About;
